2f0919293d828b30049db180175f2b9dfd25ac5a
palloc <palloc.mknet10@gmail.com>
2017-04-27 18:03:27 +0900

    use class object

--- mnist_network_parallel.py
+++ mnist_network_parallel.py
@@ -1,122 +1,127 @@
 #!/usr/bin/env python
-"""Chainer example: train a multi-layer perceptron on MNIST
-
-This is a minimal example to write a feed-forward net. It requires scikit-learn
-to load MNIST dataset.
-
-"""
-import argparse
-
 import numpy as np
 import six
-
+import data
 import chainer
+import chainer.functions as F
 from chainer import computational_graph as c
 from chainer import cuda
-import chainer.functions as F
 from chainer import optimizers
-import pprint
-import data
 from chainer import function
-
 
 
 BATCH_SIZE = 100
 N_EPOCH = 20
-N_UNITS = 1000
 GPU1 = 0
 GPU2 = 1
 
 
-# Prepare dataset
-print('load MNIST dataset')
-mnist = data.load_mnist_data()
-mnist['data'] = mnist['data'].astype(np.float32)
-mnist['data'] /= 255
-mnist['target'] = mnist['target'].astype(np.int32)
-
-N = 60000
-x_train, x_test = np.split(mnist['data'],   [N])
-y_train, y_test = np.split(mnist['target'], [N])
-N_test = y_test.size
+class ModelParalell:
+    def __init__(self):
+        pass
 
 
+    def read_dataset(self):
+        # Prepare dataset
+        print('load MNIST dataset')
+        mnist = data.load_mnist_data()
+        mnist['data'] = mnist['data'].astype(np.float32)
+        mnist['data'] /= 255
+        mnist['target'] = mnist['target'].astype(np.int32)
 
-# Model1 
-model = chainer.FunctionSet(l1=F.Linear(784, N_UNITS),
-                            l2=F.Linear(N_UNITS, 10000),
-                            l3=F.Linear(10000, 100))
+        # Number of data
+        self.N = 60000
+        self.x_train, self.x_test = np.split(mnist['data'],   [self.N])
+        self.y_train, self.y_test = np.split(mnist['target'], [self.N])
+        self.N_test = self.y_test.size
 
-# Model2
-model2 = chainer.FunctionSet(l1=F.Linear(100, 100),
-                             l2=F.Linear(100, 10))
 
-model.to_gpu(GPU1)
-model2.to_gpu(GPU2)
+    def model_define(self):
+        # Model1 
+        self.model = chainer.FunctionSet(l1=F.Linear(784, 1000),
+                                         l2=F.Linear(1000, 1000),
+                                         l3=F.Linear(1000, 100))
 
+        # Model2
+        self.model2 = chainer.FunctionSet(l1=F.Linear(100, 100),
+                                          l2=F.Linear(100, 10))
+        # Assign models to GPU
+        self.model.to_gpu(GPU1)
+        self.model2.to_gpu(GPU2)
+
+
+    # Neural net architecture
+    def forward(self, x_data, y_data, train=True):
+        x, t = chainer.Variable(x_data), chainer.Variable(y_data)
+            
+        h1 = F.dropout(F.relu(self.model.l1(x)),  train=train)
+        h2 = F.dropout(F.relu(self.model.l2(h1)), train=train)
+        h3 = F.relu(self.model.l3(h2))
+        # Change GPU1 --> GPU2
+        h3.data = cuda.to_gpu(h3.data, device=GPU2)
+        h4 = F.dropout(F.relu(self.model2.l1(h3)), train=train)
+        y = self.model2.l2(h4)
     
-# Neural net architecture
+        self.loss, self.acc = F.softmax_cross_entropy(y, t), F.accuracy(y, t)
 
 
-def forward(x_data, y_data, train=True):
-    x, t = chainer.Variable(x_data), chainer.Variable(y_data)
+    def learning(self):
+        # Setup optimizer
+        optimizer = optimizers.Adam()
+        optimizer.setup(self.model.collect_parameters())
 
-    h1 = F.dropout(F.relu(model.l1(x)),  train=train)
-    h2 = F.dropout(F.relu(model.l2(h1)), train=train)
-    h3 = F.relu(model.l3(h2))
-    # Change GPU1 --> GPU2
-    h3.data = cuda.to_gpu(h3.data, device=GPU2)
-    h4 = F.dropout(F.relu(model2.l1(h3)), train=train)
-    y = model2.l2(h4)
-    
-    return F.softmax_cross_entropy(y, t), F.accuracy(y, t)
+        # Learning loop
+        for epoch in six.moves.range(1, N_EPOCH + 1):
+            print('epoch', epoch)
+
+            # training
+            self.perm = np.random.permutation(self.N)
+            self.sum_accuracy = 0
+            self.sum_loss = 0
+            for i in six.moves.range(0, self.N, BATCH_SIZE):
+                self.x_batch = self.x_train[self.perm[i:i + BATCH_SIZE]]
+                self.y_batch = self.y_train[self.perm[i:i + BATCH_SIZE]]
+                    
+                self.x_batch = cuda.to_gpu(self.x_batch, device=GPU1)
+                self.y_batch = cuda.to_gpu(self.y_batch, device=GPU2)
+                    
+                optimizer.zero_grads()
+                self.forward(self.x_batch, self.y_batch)
+                    
+                self.loss.backward()
+                optimizer.update()
+                    
+                self.sum_loss += float(cuda.to_cpu(self.loss.data)) * len(self.y_batch)
+                self.sum_accuracy += float(cuda.to_cpu(self.acc.data)) * len(self.y_batch)
+                    
+            print('train mean loss={}, accuracy={}'.format(self.sum_loss / self.N, self.sum_accuracy / self.N))
+
+            # evaluation
+            self.sum_accuracy = 0
+            self.sum_loss = 0
+            for i in six.moves.range(0, self.N_test, BATCH_SIZE):
+                self.x_batch = self.x_test[i:i + BATCH_SIZE]
+                self.y_batch = self.y_test[i:i + BATCH_SIZE]
+
+                self.x_batch = cuda.to_gpu(self.x_batch, device=GPU1)
+                self.y_batch = cuda.to_gpu(self.y_batch, device=GPU2)
+
+                self.forward(self.x_batch, self.y_batch, train=False)
+                    
+                self.sum_loss += float(cuda.to_cpu(self.loss.data)) * len(self.y_batch)
+                self.sum_accuracy += float(cuda.to_cpu(self.acc.data)) * len(self.y_batch)
+
+            print('test  mean loss={}, accuracy={}'.format(self.sum_loss / self.N_test, self.sum_accuracy / self.N_test))
 
 
-# Setup optimizer
-optimizer = optimizers.Adam()
-optimizer.setup(model.collect_parameters())
 
-# Learning loop
-for epoch in six.moves.range(1, N_EPOCH + 1):
-    print('epoch', epoch)
 
-    # training
-    perm = np.random.permutation(N)
-    sum_accuracy = 0
-    sum_loss = 0
-    for i in six.moves.range(0, N, BATCH_SIZE):
-        x_batch = x_train[perm[i:i + BATCH_SIZE]]
-        y_batch = y_train[perm[i:i + BATCH_SIZE]]
+def main():
+    test = ModelParalell()
+    test.read_dataset()
+    test.model_define()
+    test.learning()
 
-        x_batch = cuda.to_gpu(x_batch, device=GPU1)
-        y_batch = cuda.to_gpu(y_batch, device=GPU2)
 
-        optimizer.zero_grads()
-        loss, acc = forward(x_batch, y_batch)
-
-        loss.backward()
-        optimizer.update()
-
-        sum_loss += float(cuda.to_cpu(loss.data)) * len(y_batch)
-        sum_accuracy += float(cuda.to_cpu(acc.data)) * len(y_batch)
-
-    print('train mean loss={}, accuracy={}'.format(
-        sum_loss / N, sum_accuracy / N))
-
-    # evaluation
-    sum_accuracy = 0
-    sum_loss = 0
-    for i in six.moves.range(0, N_test, BATCH_SIZE):
-        x_batch = x_test[i:i + BATCH_SIZE]
-        y_batch = y_test[i:i + BATCH_SIZE]
-
-        x_batch = cuda.to_gpu(x_batch, device=GPU1)
-        y_batch = cuda.to_gpu(y_batch, device=GPU2)
-
-        loss, acc = forward(x_batch, y_batch, train=False)
-
-        sum_loss += float(cuda.to_cpu(loss.data)) * len(y_batch)
-        sum_accuracy += float(cuda.to_cpu(acc.data)) * len(y_batch)
-
-    print('test  mean loss={}, accuracy={}'.format(
-        sum_loss / N_test, sum_accuracy / N_test))
+if __name__ == '__main__':
+    main()
