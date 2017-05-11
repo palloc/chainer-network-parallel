6099b8dadd4c1fe2c9d471b616f00de58474f823
palloc <palloc.mknet10@gmail.com>
2017-05-11 18:32:26 +0900

    add sintyoku

--- VGG/cifar100_VGG.py
+++ VGG/cifar100_VGG.py
@@ -11,10 +11,10 @@ from chainer import function
 
 
 BATCH_SIZE = 64
-N_EPOCH = 300
+N_EPOCH = 30
 GPU1 = 0
 GPU2 = 1
-CLASS_LABELS = 100
+CLASS_LABELS = 10
 
 
 class NetworkParallel:
@@ -24,7 +24,7 @@ class NetworkParallel:
     # Prepare dataset(MNIST)
     def read_dataset(self):
         print('load cifar100 dataset')
-        cifar100_train, cifar100_test = cifar.get_cifar100()
+        cifar100_train, cifar100_test = cifar.get_cifar10()
 
         # Create train data
         self.x_train = []
@@ -65,10 +65,29 @@ class NetworkParallel:
             conv3_2=L.Convolution2D(None, 256, 3, stride=1, pad=1),
             conv3_3=L.Convolution2D(None, 256, 3, stride=1, pad=1),
             conv3_4=L.Convolution2D(None, 256, 3, stride=1, pad=1),
-            bn3=L.BatchNormalization(256))
+            bn3=L.BatchNormalization(256),
+            
+            fc6=L.Linear(None, 1024),
+            fc7=L.Linear(None, 1024),
+            fc8=L.Linear(None, CLASS_LABELS))
+
 
         # Model2
         self.model2 = chainer.FunctionSet(
+            conv1_1=L.Convolution2D(3, 64, 3, stride=1, pad=1),
+            conv1_2=L.Convolution2D(None, 64, 3, stride=1, pad=1),
+            bn1=L.BatchNormalization(64),
+
+            conv2_1=L.Convolution2D(None, 128, 3, stride=1, pad=1),
+            conv2_2=L.Convolution2D(None, 128, 3, stride=1, pad=1),
+            bn2=L.BatchNormalization(128),
+
+            conv3_1=L.Convolution2D(None, 256, 3, stride=1, pad=1),
+            conv3_2=L.Convolution2D(None, 256, 3, stride=1, pad=1),
+            conv3_3=L.Convolution2D(None, 256, 3, stride=1, pad=1),
+            conv3_4=L.Convolution2D(None, 256, 3, stride=1, pad=1),
+            bn3=L.BatchNormalization(256),
+
             fc6=L.Linear(None, 1024),
             fc7=L.Linear(None, 1024),
             fc8=L.Linear(None, CLASS_LABELS))
@@ -83,26 +102,26 @@ class NetworkParallel:
 
         h = F.relu(self.model.bn1(self.model.conv1_1(x)))
         h = F.relu(self.model.bn1(self.model.conv1_2(h)))
-        h = F.dropout(h, ratio=0.5, train=train)
+        h = F.dropout(h, ratio=0.25, train=train)
         h = F.max_pooling_2d(h, 2)
-        
+
         h = F.relu(self.model.bn2(self.model.conv2_1(h)))
         h = F.relu(self.model.bn2(self.model.conv2_2(h)))
-        h = F.dropout(h, ratio=0.5, train=train)
+        h = F.dropout(h, ratio=0.25, train=train)
         h = F.max_pooling_2d(h, ksize=2, stride=2)
 
         h = F.relu(self.model.bn3(self.model.conv3_1(h)))
         h = F.relu(self.model.bn3(self.model.conv3_2(h)))
         h = F.relu(self.model.bn3(self.model.conv3_3(h)))
         h = F.relu(self.model.bn3(self.model.conv3_4(h)))
-        h = F.dropout(h, ratio=0.5, train=train)
+        h = F.dropout(h, ratio=0.25, train=train)
         h = F.max_pooling_2d(h, 2, stride=2)
 
         # Change GPU1 --> GPU2
         h.data = cuda.to_gpu(h.data, device=GPU2)
 
-        h = F.dropout(F.relu(self.model2.fc6(h)), train=train, ratio=0.5)
-        h = F.dropout(F.relu(self.model2.fc7(h)), train=train, ratio=0.5)
+        h = F.dropout(F.relu(self.model2.fc6(h)), train=train, ratio=0.25)
+        h = F.dropout(F.relu(self.model2.fc7(h)), train=train, ratio=0.25)
         y = self.model2.fc8(h)
 
         self.loss, self.acc = F.softmax_cross_entropy(y, t), F.accuracy(y, t)
